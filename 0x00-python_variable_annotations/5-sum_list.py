df87bda4a7366a2560447b8bf91cd15d9b4b632a
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-08-16 06:06:22 +0000

    tasks

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-from typing import List, Dict
+from typing import List
 """
 Type-annotated function
 """
 
 
 def sum_list(input_list: List[float]) -> float:
-    sum: float = 0
+    sum: float = 0.0
     for item in input_list:
         sum += item
     return sum
