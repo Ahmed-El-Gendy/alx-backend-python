4f797ac7f183359ce6395d7c3d998ea4c0f27b6f
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-08-16 06:10:27 +0000

    tasks

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
-from typing import Callable
 """
 Type-annotated function
 """
+from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """make mul function"""
     def multiplier_function(value: float) -> float:
+        """Mul function"""
         return value * multiplier
     return multiplier_function
