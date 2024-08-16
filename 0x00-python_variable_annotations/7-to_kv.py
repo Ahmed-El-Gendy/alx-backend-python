b58d92372c8fe93ad7e8a7888af241f80228a6a3
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-08-16 06:09:07 +0000

    tasks

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-from typing import Tuple, Union
 """
 Type-annotated function
 """
+from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
