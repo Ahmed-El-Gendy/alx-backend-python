91d89526268a800d666c48f9174acdf8b999ea30
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-08-20 06:52:16 +0000

    task2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -4,8 +4,9 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int = 10) -> float:
     """Waits for delays"""
+    start: float
     start = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     total = time.perf_counter() - start
