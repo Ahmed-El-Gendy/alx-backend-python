2f2ef4bbfb92fda1ba8113f78fec8e8e8486a8b9
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-09-10 14:22:23 +0000

    task3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -86,9 +86,9 @@ class TestMemoize(unittest.TestCase):
         with patch.object(
                 TestClass,
                 "a_method",
-                return_value=lambda: 42,
-                ) as memo_fxn:
+                return_value=42,
+                ) as mock_method:
             test_class = TestClass()
             self.assertEqual(test_class.a_property(), 42)
             self.assertEqual(test_class.a_property(), 42)
-            memo_fxn.assert_called_once()
+            mock_method.assert_called_once()
