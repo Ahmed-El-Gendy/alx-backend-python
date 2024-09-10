8885313ef9efeb3adf776ecac30dfb00141b41be
Ahmed El Gendy <125159880+Ahmed-El-Gendy@users.noreply.github.com>
2024-09-10 14:18:19 +0000

    task3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -75,7 +75,7 @@ class TestMemoize(unittest.TestCase):
     """
     def test_memoize(self) -> None:
         """
-        Test that the memoize function memoizes correctly
+        Test that the function memoizes the result of a method
         """
         class TestClass:
             def a_method(self):
@@ -84,13 +84,12 @@ class TestMemoize(unittest.TestCase):
             @memoize
             def a_property(self):
                 return self.a_method()
-
         with patch.object(
                 TestClass,
-                'a_method',
+                "a_method",
                 return_value=lambda: 42,
                 ) as memo_fxn:
-            test = TestClass()
-            self.assertEqual(test.a_property(), 42)
-            self.assertEqual(test.a_property(), 42)
+            test_class = TestClass()
+            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property(), 42)
             memo_fxn.assert_called_once()
